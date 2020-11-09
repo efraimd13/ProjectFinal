6162c4219d4198931f4ae970b673bad3b6d99b11
efraimd13 <efraimdahlan1237@gmail.com>
2020-11-09 22:56:39 +0800

    Exercise 07

---
+++ pertemuan_23/a.js
@@ -0,0 +1,93 @@
+//  1. VARIABEL
+
+//let & const
+
+let _umur = 25;
+_umur=68;
+let nilai=98.4;
+console.log(nilai);
+let $nama = "John";
+console. log(_umur);
+console.log($nama);
+let sudahMenikah = true;
+console.log(sudahMenikah);
+
+let alamat;
+console.log(alamat)
+
+
+const tahunLahir = 2002;
+console.log(tahunLahir);
+
+// Const digunakan untuk data yg tetap atau variabel yg sifat nya tidak bisa diubah
+
+
+
+
+
+//  2. OPERATOR
+
+//Arithmetic Operator (*,=,+,-)
+
+
+let bilangan1 = 99;
+let bilangan2 = 23;
+console.log(bilangan1 + bilangan2);
+
+
+let bilangan3 =  9;
+let bilangan4 = 3;
+let hasil= bilangan3+bilangan4;
+console.log(hasil);
+
+let nama="Aim";
+let namaB="Riva";
+console.log(nama + " "+ namaB);
+
+
+let angka1 = 10;
+let angka2 ="10";
+
+let hasilper = angka1 == angka2;
+let hasilper2 = angka1 === angka2;
+console.log(hasilper);
+console.log(hasilper2);
+
+let angkaBaru = 5;
+let hasilBaru = angkaBaru < 0 ? "Negatif" : "Positif";
+console.log(hasilBaru);
+
+
+let kondisi = 1<2;
+let kondisi2 = true;
+
+console.log(kondisi || kondisi2);
+
+console.log (!kondisi);
+
+let beratBadan = prompt("Masukan Berat > ");
+console.log("Berat yg diinput > "+beratBadan);3
+let tinggiBadan = prompt("Masukan Tinggi > ");
+console.log("Tinggi yg diinput > "+tinggiBadan);
+let tinggi= tinggiBadan/10;
+let tinggiTotal = tinggi*tinggi;
+let hasilBMI = beratBadan / tinggiTotal;
+console.log("BMI Anda adalah > "+hasilBMI);
+ // BMI = berat (kg) / tinggi x tinggi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
